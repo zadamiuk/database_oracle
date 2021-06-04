07db6b4b0b3fd1489194bf8096b47bfc2fb9f77c
Zuzanna Adamiuk <z.adamiuk@stud.elka.pw.edu.pl>
2021-06-04 15:45:38 +0200

    dodane fragmenty kodu zgodnie z przykladem prowadzacej, ale cos nie dziala :(

--- src/bd1projekt/Main.java
+++ src/bd1projekt/Main.java
@@ -1,8 +1,73 @@
 package bd1projekt;
 
+import oracle.jdbc.datasource.OracleDataSource;
+
+import java.io.IOException;
+import java.sql.*;
+
+// na podstawie kodow od prowadzacej
+
 public class Main {
 
+    Connection conn;
+
     public static void main(String[] args) {
-	// write your code here
+
+        Main app = new Main();
+
+        try {
+            app.setConnection();
+            //app.showPatients();
+            app.closeConnection();
+        }
+        catch (SQLException | IOException eSQL) {
+            System.out.println("Blad SQL " + eSQL.getMessage());
+        }
     }
+
+    public void setConnection() throws SQLException, IOException {
+        String host = "ora4.ii.pw.edu.pl";
+        String username = "z56";
+        String password = "brinkc";
+        String port = "1521";
+        String serviceName = "pdb1.ii.pw.edu.pl";
+        String connectionString = String.format(
+                "jdbc:oracle:thin:%s/%s@//%s:%s/%s",
+                username, password, host, port, serviceName);
+
+        OracleDataSource ods;
+        ods = new OracleDataSource();
+
+        ods.setURL(connectionString);
+        conn = ods.getConnection(); // nawiazujemy polaczenie z BD
+
+        DatabaseMetaData meta = conn.getMetaData();
+
+        System.out.println("Polaczenie do bazy danych zostalo nawiazane.");
+        System.out.println("Baza danych:" + " " + meta.getDatabaseProductVersion());
+    }
+
+    public void closeConnection() throws SQLException {
+        conn.close();
+        System.out.println("Polaczenie z baza zostalo poprawnie zamkniete.");
+    }
+
+    public void showPatients() throws SQLException {
+        System.out.println("Lista pacjentow:");
+
+        Statement stat = conn.createStatement();
+
+        // wydajemy zapytanie oraz zapisujemy rezultat w obiekcie typu ResultSet
+        ResultSet rs = stat.executeQuery("SELECT imie, nazwisko FROM pacjenci");
+
+        System.out.println("---------------------------------");
+        // iteracyjnie odczytujemy rezultaty zapytania
+        while (rs.next())
+            System.out.println(rs.getString(1) + " " + rs.getString(2));
+        System.out.println("---------------------------------");
+
+        rs.close();
+        stat.close();
+    }
+
 }
