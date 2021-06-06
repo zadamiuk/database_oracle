d7c3cbd6ed357c6aaee7aa9e796533a14fcc5e49
Natalia Joanna Dominik <n.dominik@stud.elka.pw.edu.pl>
2021-06-06 13:40:27 +0200

    dodane nowe metody

--- src/bd1projekt/Main.java
+++ src/bd1projekt/Main.java
@@ -1,6 +1,6 @@
 package bd1projekt;
 
-import oracle.jdbc.datasource.OracleDataSource;
+import oracle.jdbc.pool.OracleDataSource;
 
 import java.io.IOException;
 import java.sql.*;
@@ -17,12 +17,15 @@ public class Main {
 
         try {
             app.setConnection();
-            //app.showPatients();
+            app.showPatients();
+            app.showDoctors("Kardiologia");
+            app.showOperations(2019);
             app.closeConnection();
         }
         catch (SQLException | IOException eSQL) {
             System.out.println("Blad SQL " + eSQL.getMessage());
         }
+        
     }
 
     public void setConnection() throws SQLException, IOException {
@@ -64,6 +67,50 @@ public class Main {
         // iteracyjnie odczytujemy rezultaty zapytania
         while (rs.next())
             System.out.println(rs.getString(1) + " " + rs.getString(2));
+        System.out.println("---------------------------------");
+
+        rs.close();
+        stat.close();
+    }
+    
+    public void showDoctors(String specjalizacja) throws SQLException {
+        System.out.println("Lista lekarzy ze specjalizacja " + specjalizacja + ":");
+
+        Statement stat = conn.createStatement();
+
+        // wydajemy zapytanie oraz zapisujemy rezultat w obiekcie typu ResultSet
+        ResultSet rs = stat.executeQuery("SELECT l.imie, l.nazwisko "
+        								+ "FROM lekarze l "
+        								+ "INNER JOIN lekarze_specjalizacje ls USING(id_lekarza) "
+        								+ "INNER JOIN specjalizacje s USING(id_specjalizacji)"
+        								+ "WHERE s.nazwa = '" + specjalizacja + "'");
+
+        System.out.println("---------------------------------");
+        // iteracyjnie odczytujemy rezultaty zapytania
+        while (rs.next())
+            System.out.println(rs.getString(1) + " " + rs.getString(2));
+        System.out.println("---------------------------------");
+
+        rs.close();
+        stat.close();
+    }
+    
+    public void showOperations(int rok) throws SQLException {
+        System.out.println("Lista zabiegów w roku " + rok + ":");
+
+        Statement stat = conn.createStatement();
+
+        // wydajemy zapytanie oraz zapisujemy rezultat w obiekcie typu ResultSet
+        ResultSet rs = stat.executeQuery("SELECT hz.data_zabiegu, z.nazwa, p.imie, p.nazwisko "
+        								+ "FROM historia_zabiegow hz "
+        								+ "INNER JOIN zabiegi z USING(id_zabiegu) "
+        								+ "INNER JOIN pacjenci p USING(id_pacjenta) "
+        								+ "WHERE EXTRACT(YEAR FROM data_zabiegu) = 2019");
+
+        System.out.println("---------------------------------");
+        // iteracyjnie odczytujemy rezultaty zapytania
+        while (rs.next())
+            System.out.println("Data zabiegu: " + rs.getString(1) + "\nNazwa zabiegu: " + rs.getString(2) + "\nImiê i nazwisko pacjenta: " + rs.getString(3) + " " + rs.getString(4) + "\n");
         System.out.println("---------------------------------");
 
         rs.close();
